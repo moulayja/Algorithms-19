8ef4927eed22db0e2f80a6364591bfb409c90fc0
Jamal <moulayjam@gmail.com>
2020-08-24 11:13:35 -0400

    Changing a String into camelCase [Solution]

---
+++ Changing a String into camelCase.js
@@ -0,0 +1,26 @@
+
+/*
+
+Using Camel Case (or camelCase) is where the first word is in lower case, and all words after it have their first letter capitalised. Note that there are no spaces in between words!
+
+Create a function that takes a string and returns it back in camelCase.
+
+Examples
+camelCasing("Hello World") ➞ "helloWorld"
+
+camelCasing("snake_case") ➞ "snakeCase"
+
+camelCasing("low high_HIGH") ➞ "lowHighHigh"
+
+Notes
+You need to remove all spaces and underscores.
+There will be no numbers in inputs.
+
+*/
+
+const camelCasing = string => {
+	return string.split(/[ _]/g).map((w,i) => (!i ?
+		w.toLowerCase(): w[0].toUpperCase() + w.slice(1).toLowerCase()
+	)).join("");
+}
+
